d17210b4c56c484d8bd8ddee50b219dbddaaf3a5
Medimuz <pava312@gmail.com>
2023-05-10 18:15:37 +0700

    slightly modified

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ while True:
     user_choice = input('Type rock, paper, scissors or q for quit: ').lower()
     if user_choice == "q":
         break
+
     if user_choice not in variations:
         continue
+
     else:
         print("You choose", user_choice)
-    comp_choice = random.randint(0, 2)
-    print(comp_choice)
-    if user_choice == 'rock' and comp_choice == 1:
+    randon_number = random.randint(0, 2)
+    comp_choice = variations[randon_number]
+
+    if user_choice == 'rock' and comp_choice == 'scissors':
         print('You won!')
         user_score += 1
-    elif user_choice == 'paper' and comp_choice == 0:
+    elif user_choice == 'paper' and comp_choice == 'rock':
         print('You won!')
         user_score += 1
-    elif user_choice == 'scissors' and comp_choice == 2:
+    elif user_choice == 'scissors' and comp_choice == 'paper':
         print('You won!')
         user_score += 1
     else:
         print("You lost!")
         comp_score += 1
 print(f"total score: You - {user_score}, Comp - {comp_score}")
-print("see ya!")
+print("See ya!")
 
